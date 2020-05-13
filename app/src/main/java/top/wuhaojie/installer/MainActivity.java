eed1e9c81aa087cb77c66b9d62ae7c6826783533
刘浩 <liuhao10@renmaitech.com>
2020-05-13 16:40:53 +0800

    init

--- app/src/main/java/top/wuhaojie/installer/MainActivity.java
+++ app/src/main/java/top/wuhaojie/installer/MainActivity.java
@@ -22,11 +22,11 @@ import top.wuhaojie.installerlibrary.AutoInstaller;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     //    private static String APK_FILE_PATH = "/storage/emulated/0/Android/data/yunzhe.plugin.staticinstaller/files/Download/ryh.apk";
     private String APK_FILE_PATH = "/storage/emulated/0/Android/data/yunzhe.plugin.staticinstaller/files/Download/qz.apk";
-
     //    public static final String APK_FILE_PATH = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "Download" + File.separator + "test.apk";
+
     public static final String CACHE_FILE_PATH = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "Download";
+
     public static final String APK_URL = "https://54a0bf2343ff38bdc347780545bd8c9e.dd.cdntips.com/imtt.dd.qq.com/16891/apk/364F479E2C19B0961A84BA9D037DF00A.apk?mkey=5eba481024707b0b&f=24c3&fsname=com.renmai.easymoney_2.3.0_18.apk&csr=1bbd&cip=36.112.93.254&proto=https";
-//    public static final String APK_URL = "http://192.168.1.185:8080/app-release.apk";
     private ProgressDialog mProgressDialog;
 
     @Override
